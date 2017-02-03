98660cb44d34027e36374228c90d89881faedc76
Imad Pasha <prappleizerjohn@gmail.com>
2017-02-02 23:56:27 -0800

    fixed typo

--- tutorials/hw2/ok_tests/q03.py
+++ tutorials/hw2/ok_tests/q03.py
@@ -8,9 +8,9 @@ test = {
           'code': r"""
           >>> quad_solve(2,3,4)==None
           True
-          >>> quad_solve(2,5,1)==(3.0, -5.5)
+          >>> quad_solve(4,5,1)==(-0.25, -1.0)
           True
-          >>> quad_solve(1,5,2)==(6.0, -11.0)
+          >>> quad_solve(2,5,2)==(-0.5, -2.0)
           True
           """,
           'hidden': False,
