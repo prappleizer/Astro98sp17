04515d3878cfff29110250f3f10115645253f12f
Imad Pasha <prappleizerjohn@gmail.com>
2017-02-09 18:25:44 -0800

    Added tutorial 3

---
+++ tutorials/hw3/ok_tests/q01.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> peak_finder(signal)== [1194, 1254, 1289, 1321, 1344, 1390, 1422, 1483, 1534, 1558, 1612, 1635, 1697, 1753, 1799, 1833, 1891, 1915, 2040, 2044]
+          True
+          >>> type(peak_finder(signal))== list
+          True
+          """,
+          'hidden': True,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
